e724b7d2b806c26699dad4ce24c5212bcefb9cc1
shaznan123 <92423708+shaznan123@users.noreply.github.com>
2022-09-15 16:41:44 +0530

    Make Table Component Responsive

--- src/Component/Common/Table/index.tsx
+++ src/Component/Common/Table/index.tsx
@@ -1,68 +1,141 @@
 import DataTable from "react-data-table-component";
-import React from "react";
+import React, { FC, useMemo } from "react";
+import { FaEdit } from "react-icons/fa";
+import { BsFillEyeFill } from "react-icons/bs";
+import {
+  AiOutlineStar,
+  AiFillStar,
+  AiFillDelete,
+} from "react-icons/ai";
+import RenderCustomPagination from "./Pagination";
+import {
+  tableOptions,
+  TableColumnTitles,
+} from "../../../Constants/constants";
 
-const Table = () => {
-  const columns = [
-    {
-      name: "Prescription Name",
-      selector: (row: any) =>
-        row.prescriptionName,
-    },
-    {
-      name: "Doctor Name",
-      selector: (row: any) => row.doctorName,
-    },
-    {
-      name: "Date Prescribe",
-      selector: (row: any) => row.datePrescribed,
-    },
-    {
-      name: "Actions",
-      selector: (row: any) => row.actions,
-    },
-  ];
+interface IColumns {
+  name: string;
+  selector: any;
+  sortable?: boolean;
+  type?: string[];
+}
 
-  const data = [
-    {
-      id: 1,
-      prescriptionName: "Beetlejuice",
-      doctorName: "1988",
-      datePrescribed: "10.10.2022",
-      actions: "Test Action",
+interface ITable {
+  data: any;
+  columns: IColumns[];
+}
+
+const Table: FC<ITable> = ({ data, columns }) => {
+  const tempIconStyles = {
+    marginRight: "0.5rem",
+    cursor: "pointer",
+  };
+
+  const renderIcons = (
+    checkIfPropertyExists: Function
+  ) => (
+    <div style={{ display: "flex" }}>
+      {checkIfPropertyExists(
+        tableOptions.VIEW
+      ) && (
+        <BsFillEyeFill
+          fontSize={18}
+          style={tempIconStyles}
+        />
+      )}
+      {checkIfPropertyExists(
+        tableOptions.EDIT
+      ) && (
+        <FaEdit
+          fontSize={18}
+          style={tempIconStyles}
+        />
+      )}
+      {checkIfPropertyExists(
+        tableOptions.DELETE
+      ) && (
+        <AiFillDelete
+          fontSize={18}
+          style={tempIconStyles}
+        />
+      )}
+      {checkIfPropertyExists(
+        tableOptions.STAR
+      ) && (
+        <AiOutlineStar
+          fontSize={18}
+          style={tempIconStyles}
+        />
+      )}
+    </div>
+  );
+
+  const formatColumnData = useMemo(() => {
+    const customColumns = columns?.map((item) => {
+      const checkIfPropertyExists = (
+        prop: string
+      ) => item?.type?.includes(prop);
+      const columnTitle = item?.name;
+      if (
+        columnTitle === TableColumnTitles.ACTIONS
+      ) {
+        return {
+          ...item,
+          cell: () =>
+            renderIcons(checkIfPropertyExists),
+        };
+      }
+      if (
+        columnTitle ===
+        TableColumnTitles.NEXT_REFILL_DATE
+      ) {
+        return {
+          ...item,
+          cell: (row: any) => (
+            <>
+              <span
+                style={{
+                  backgroundColor: "blue",
+                  color: "blue",
+                  minWidth: "5px",
+                  marginRight: "0.5em",
+                }}
+              >
+                .
+              </span>
+              {row?.nextRefillDate}
+            </>
+          ),
+        };
+      }
+
+      return item;
+    });
+    return customColumns;
+  }, [columns]);
+
+  const customStyles = {
+    headCells: {
+      style: {
+        paddingLeft: "8px",
+        paddingRight: "8px",
+        backgroundColor: "lightBlue",
+      },
     },
-    {
-      id: 2,
-      prescriptionName: "Beetlejuice",
-      doctorName: "1988",
-      datePrescribed: "10.10.2022",
-      actions: "Test Action",
-    },
-    {
-      id: 3,
-      prescriptionName: "Beetlejuice",
-      doctorName: "1988",
-      datePrescribed: "10.10.2022",
-      actions: "Test Action",
-    },
-    {
-      id: 4,
-      prescriptionName: "Beetlejuice",
-      doctorName: "1988",
-      datePrescribed: "10.10.2022",
-      actions: "Test Action",
-    },
-    {
-      id: 5,
-      prescriptionName: "Beetlejuice",
-      doctorName: "1988",
-      datePrescribed: "10.10.2022",
-      actions: "Test Action",
-    },
-  ];
+  };
 
   return (
     <>
-      <DataTable columns={columns} data={data} />
+      <DataTable
+        columns={formatColumnData}
+        data={data}
+        pagination
+        selectableRows
+        customStyles={customStyles}
+        paginationComponent={
+          RenderCustomPagination
+        }
+      />
     </>
   );
 };
--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -6,10 +6,49 @@ import {
 import ProtectedRoute from "./ProtectedRoute";
 import Table from "../Component/Common/Table";
 
+const data = [
+  {
+    id: 1,
+    prescriptionName: "Diabetes",
+    doctorName: "Dr Shenelle Moses",
+    datePrescribed: "08.10.2022",
+    nextRefillDate: "08.20.2021",
+  },
+];
+
+const columns = [
+  {
+    name: "Prescription Name",
+    selector: (row: any) => row.prescriptionName,
+    sortable: true,
+  },
+  {
+    name: "Doctor Name",
+    selector: (row: any) => row.doctorName,
+    sortable: true,
+  },
+  {
+    name: "Next Refill Date",
+    selector: (row: any) => row.nextRefillDate,
+    sortable: true,
+    // type: ["refill"],
+  },
+  {
+    name: "Date Prescribe",
+    selector: (row: any) => row.datePrescribed,
+    sortable: true,
+  },
+  {
+    name: "Actions",
+    selector: (row: any) => row.actions,
+    type: ["view", "edit", "delete", "star"],
+  },
+];
+
 //R&D
 const RenderResearchComponent = () => (
   <>
-    <Table />
+    <Table data={data} columns={columns} />
   </>
 );
 
